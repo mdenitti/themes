14f350392146ce1c3cd1a8852af20cc8f8c70848
mdenitti <massimodn77@gmail.com>
2025-03-11 19:02:41 +0100

    basic template live

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -1,3 +1,7 @@
-@foreach ($cities as $city)
-    {{$city->name}} <hr>
-@endforeach
+@extends('layout')
+@section('title','My cities')
+@section('content')
+    @foreach($cities as $city)
+        {{$city->name}}<hr>
+    @endforeach
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,4 @@
+@include('header')
+    <h1>@yield('title')</h1>
+    @yield('content')
+@include('footer')
