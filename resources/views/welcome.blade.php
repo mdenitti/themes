7fde82482e55d86ef8c3a9c37cdc71099330a0ac
mdenitti <massimodn77@gmail.com>
2025-04-17 14:32:15 +0200

    Remove product listing from welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,5 @@
 @extends('template')
 @section('title','Unieke steden in de wereld')
 @section('content')
-    @php
-    $products = App\Models\Category::find(1)->products; // Collection of products
-    @endphp
-    @foreach ($products as $product) 
-        {{$product->name}} <hr>
-    @endforeach
+   
 @endsection
