88b43c935261b63fae0aa1b76e5da81f04a1173e
mdenitti <massimodn77@gmail.com>
2025-05-13 11:08:20 +0200

    Update User.php with HasApiTokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Profile;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
