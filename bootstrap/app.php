0e3ba2e1e9fd0688e08eb266b0cece95282071df
mdenitti <massimodn77@gmail.com>
2025-05-14 08:45:27 +0200

    Refactor middleware configuration to include authentication and stateful API request handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-       
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+        ]);
+
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
