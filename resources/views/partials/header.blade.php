1188030a0be1394b25e2eba49faf715cdf9b1b1e
mdenitti <massimodn77@gmail.com>
2025-05-20 11:05:34 +0000

    Remove Bootstrap CSS/JS includes from Blade templates (header, footer)

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,10 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>De Blauwe Vogel Themadagen - @yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  <link rel="icon" href="{{asset('favicon.ico')}}">
-  <link href="{{asset('style.css')}}" rel="stylesheet"></head>
+    <link rel="icon" href="{{asset('favicon.ico')}}">
+    <link href="{{asset('style.css')}}" rel="stylesheet"></head>
   <body
     x-data="{ page: 'blog-single', 'darkMode': true, 'stickyMenu': false, 'navigationOpen': false, 'scrollTop': false }"
     x-init="
